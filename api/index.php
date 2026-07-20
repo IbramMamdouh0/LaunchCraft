bdeb7bd154b1623a0a4019e8ad95feeccdabb18d
Ibram Mamdouh <IbramMamdouh0@users.noreply.github.com>
2026-07-20 17:43:00 +0300

    fix: set correct base bootstrap path to /tmp/bootstrap

--- api/index.php
+++ api/index.php
@@ -30,7 +30,7 @@ try {
     /** @var \Illuminate\Foundation\Application $app */
     $app = require __DIR__ . '/../bootstrap/app.php';
 
-    $app->useBootstrapPath('/tmp/bootstrap/cache');
+    $app->useBootstrapPath('/tmp/bootstrap');
 
     $app->register(\Illuminate\Filesystem\FilesystemServiceProvider::class);
     $app->register(\Illuminate\View\ViewServiceProvider::class);
