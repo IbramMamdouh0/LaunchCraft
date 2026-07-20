8fb1c7ba0b1c0fc29d5e6df07904e31ea4303765
Ibram Mamdouh <YOUR_GITHUB_EMAIL>
2026-07-20 17:26:23 +0300

    fix: force register ViewServiceProvider in AppServiceProvider register hook

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,9 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        if (! $this->app->bound('view')) {
+            $this->app->register(\Illuminate\View\ViewServiceProvider::class);
+        }
     }
 
     public function boot(): void
