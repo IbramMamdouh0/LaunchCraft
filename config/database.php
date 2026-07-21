f4bcef9d99bce3de8d16b515fa5e9a3bf66076f0
Ibram Mamdouh <IbramMamdouh0@users.noreply.github.com>
2026-07-21 13:02:42 +0300

    fix: complete serverless routing, cors, and mongodb setup

--- config/database.php
+++ config/database.php
@@ -117,14 +117,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('MONGODB_URI'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE', 'launchcraft'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-            'options' => [
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
-            ],
         ],
 
     ],
